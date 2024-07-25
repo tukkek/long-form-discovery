ad204b83041159fb610f8f2667fb09630fbef1c5
tukkek <tukkek@gmail.com>
2024-07-24 21:20:42 -0300

    Remove: let's play

--- pages.js
+++ pages.js
@@ -13,7 +13,7 @@ var audiobook=['audio-book',]
 var podcasts=[new Link('Trending episodes','https://www.podchaser.com/episodes/trending'),
                       new Link('Trending podcasts','https://www.podchaser.com/podcasts/trending?ranking_period=month'),]
 var generic=['class','debate','interview','lecture','live music','podcast',]
-var topics=['lore','philosophy','role-playing session','movie review',"let's play",
+var topics=['lore','philosophy','role-playing session','movie review',
             new Link('New speed-runs','https://tukkek.github.io/new-speedruns/'),]
 var platforms=[new Link('You Tube watch-later','https://www.youtube.com/playlist?list=WL'),
                       new Link('Amazon watch-list','https://www.primevideo.com/mystuff/watchlist'),
