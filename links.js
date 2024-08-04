e43f767f394c242a6fa9fe051a54a43309560564
tukkek <tukkek@gmail.com>
2024-08-04 11:40:08 -0300

    Remove search: war-game

--- links.js
+++ links.js
@@ -14,7 +14,7 @@ var audiobook=['audio-book',]
 var podcasts=[new Link('Trending episodes','https://www.podchaser.com/episodes/trending'),
                       new Link('Trending podcasts','https://www.podchaser.com/podcasts/trending?ranking_period=month'),]
 var generic=['debate','interview','lecture','podcast',]
-var topics=['philosophy','movie review','urban exploration','war-game',
+var topics=['philosophy','movie review','urban exploration',
             new Link('New speed-runs','https://tukkek.github.io/new-speedruns/'),]
 var platforms=[new Link('You Tube watch-later','https://www.youtube.com/playlist?list=WL'),
                       new Link('Amazon watch-list','https://www.primevideo.com/mystuff/watchlist'),
