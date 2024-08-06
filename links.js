63cc4f93e1b2d9b1529d783344e3135456b4150b
tukkek <tukkek@gmail.com>
2024-08-06 14:25:52 -0300

    Remove Spotify episodes

--- links.js
+++ links.js
@@ -19,8 +19,7 @@ var topics=['philosophy','movie review','urban exploration',
 var platforms=[new Link('You Tube watch-later','https://www.youtube.com/playlist?list=WL'),
                       new Link('Amazon watch-list','https://www.primevideo.com/mystuff/watchlist'),
                       new Link('You Tube music library','https://music.youtube.com/library'),
-                      new Link('Pod Chaser feed','https://www.podchaser.com/myfeed'),
-                      new Link('Spotify episodes','https://open.spotify.com/collection/your-episodes'),]
+                      new Link('Pod Chaser feed','https://www.podchaser.com/myfeed'),]
 var music=[new Link('Sound Cloud','https://soundcloud.com/discover')]
 
 function search(topics){
