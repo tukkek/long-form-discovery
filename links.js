f349f42b31a7e858ca806996e5f1fa18f31ef810
Alex Henry <tukkek@gmail.com>
2024-08-22 00:00:00 +0000

    Remove more You Tube and refactoring.

--- links.js
+++ links.js
@@ -10,19 +10,18 @@ class Link{
   }
 }
 
-var audiobook=['audio-book',]
-var podcasts=[new Link('Trending episodes','https://www.podchaser.com/episodes/trending'),
+var podcasts=['Podcasts',new Link('Trending episodes','https://www.podchaser.com/episodes/trending'),
                       new Link('Trending podcasts','https://www.podchaser.com/podcasts/trending?ranking_period=month'),]
-/*var generic=['debate','interview','lecture','podcast',]
-var topics=['philosophy','movie review',
-            new Link('New speed-runs','https://tukkek.github.io/new-speedruns/'),]*/
-var platforms=[new Link('You Tube watch-later','https://www.youtube.com/playlist?list=WL'),
+var topics=['Videos',
+            new Link('New speed-runs','https://tukkek.github.io/new-speedruns/'),]
+var platforms=['Platform-specific',new Link('You Tube watch-later','https://www.youtube.com/playlist?list=WL'),
                       new Link('Amazon watch-list','https://www.primevideo.com/mystuff/watchlist'),
                       new Link('You Tube music library','https://music.youtube.com/library'),
                       new Link('Pod Chaser feed','https://www.podchaser.com/myfeed'),
                       new Link('Pod Chaser bookmarks','https://www.podchaser.com/profile/bookmarks/episodes'),]
-var music=[new Link('Sound Cloud','https://soundcloud.com/discover')]
-var trending=[new Link('United States','https://trends.google.com/trends/explore?date=now%207-d&geo=US&gprop=youtube&hl=en-GB'),
+var music=['Music',new Link('Sound Cloud','https://soundcloud.com/discover')]
+var trending=['Trending topics',
+              new Link('United States','https://trends.google.com/trends/explore?date=now%207-d&geo=US&gprop=youtube&hl=en-GB'),
               new Link('Mexico','https://trends.google.com/trends/explore?date=now%207-d&geo=MX&gprop=youtube&hl=en-GB'),
               new Link('France','https://trends.google.com/trends/explore?date=now%207-d&geo=FR&gprop=youtube&hl=en-GB'),
               new Link('Brazil','https://trends.google.com/trends/explore?date=now%207-d&geo=BR&gprop=youtube&hl=en-GB'),]
@@ -66,13 +65,8 @@ function list(section,links){
 function encase(text){return text[0].toUpperCase()+text.slice(1).toLowerCase()}
 
 export function setup(){
-  list('Audio-books',search(audiobook ))
   for(let c of COUNTRIES) for(let chart of CHARTS)
     podcasts.push(new Link(`${encase(chart)} ${c.toUpperCase()} chart`,`https://www.podchaser.com/charts/${chart}/${c}/top%20podcasts`))
-  list('Music',music)
-  list('Platform-specific',platforms)
-  list('Podcasts',podcasts)
-  // list('Videos',search(generic))
-  // list('Videos (topics)',search(topics))
-  list('Trending topics',trending)
+  for(let category of [music,platforms,podcasts,topics,trending])
+    list(category[0],search(category.slice(1)))
 }
